e9d5aab1cb36cd2346535447c4dc83f47a03e131
FelipeCassano <134609406+FelipeCassano@users.noreply.github.com>
2024-03-07 19:08:02 -0300

    PageTitle criada

--- podologos/src/components/PageTitle/index.tsx
+++ podologos/src/components/PageTitle/index.tsx
@@ -1,5 +1,5 @@
 import React, { View, TouchableOpacity, Text, Image } from "react-native";
-import Seta from "../../assets/Seta.svg";
+import Seta from "../../assets/ArrowLeft.png";
 
 interface Props {
   text?: string;
@@ -7,11 +7,17 @@ interface Props {
 
 export default function PageTitle({ text }: Props) {
   return (
-    <View className="flex items-center w-full">
-      <TouchableOpacity>
-        <Image source={Seta}></Image>
-      </TouchableOpacity>
-      <Text className="text-azul_escuro text-[24px]">{text}</Text>
+    <View className="flex flex-row items-center  w-full">
+
+      <View className="flex ">
+        <TouchableOpacity>
+          <Image className="w-8 h-8" source={Seta}></Image>
+        </TouchableOpacity>
+      </View>
+
+      <View className="flex items-center justify-center w-[100%] ">
+        <Text className="text-azul_escuro mr-8 text-[24px]">{text}</Text>
+      </View>
     </View>
   );
 }
