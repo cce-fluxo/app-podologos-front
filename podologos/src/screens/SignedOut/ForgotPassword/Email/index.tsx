10d4a0d6f404340d65aac2fe559747e339e1d956
FelipeCassano <134609406+FelipeCassano@users.noreply.github.com>
2024-03-07 18:42:12 -0300

    Alterações ForgotPassword(email)

--- podologos/src/screens/SignedOut/ForgotPassword/Email/index.tsx
+++ podologos/src/screens/SignedOut/ForgotPassword/Email/index.tsx
@@ -1,11 +1,28 @@
 import { ArrowLeft } from "lucide-react-native";
 import { SafeAreaView, Text, View } from "react-native";
+import Input from "../../../../components/Inputs";
+import { Button } from "../../../../components/Button";
 
 function Email() {
   return (
-    <SafeAreaView className="flex items-center justify-center">
-      <Text>OLÁ</Text>
-    </SafeAreaView>
+    <View className="flex-col items-center justify-between ">
+      <View></View>
+
+      <View className="flex h-screen w-screen justify-center items-center ">
+        <Text className="text-[#4F5450] font-bold text-[24px] mb-4">
+          Insira seu e-mail
+        </Text>
+        <Text className="text-[14px] m-4">
+          Informe o email cadastrado e um email com as instruções de recuperação
+          será enviado.{" "}
+        </Text>
+        <Input placeholder="Email"></Input>
+      </View>
+
+      <View className="flex">
+        <Button></Button>
+      </View>
+    </View>
   );
 }
 
