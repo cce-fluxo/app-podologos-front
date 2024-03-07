fbaf8a2f39916ffb11f1a90a0a57688f2441afde
Luiis96 <134606190+Luiis96@users.noreply.github.com>
2024-03-07 19:07:08 -0300

    Componente Title funcionando

--- podologos/src/components/PageTitle/index.tsx
+++ podologos/src/components/PageTitle/index.tsx
@@ -1,5 +1,5 @@
 import React, { View, TouchableOpacity, Text, Image } from "react-native";
-import Seta from "../../assets/Seta.svg";
+import Seta from "../../assets/Seta.png";
 
 interface Props {
   text?: string;
@@ -7,11 +7,12 @@ interface Props {
 
 export default function PageTitle({ text }: Props) {
   return (
-    <View className="flex items-center w-full">
-      <TouchableOpacity>
+    <View className="flex flex-row items-center justify-between w-full px-4">
+      <TouchableOpacity className="w-[10px]">
         <Image source={Seta}></Image>
       </TouchableOpacity>
       <Text className="text-azul_escuro text-[24px]">{text}</Text>
+      <View className="w-[10px]"></View>
     </View>
   );
 }
