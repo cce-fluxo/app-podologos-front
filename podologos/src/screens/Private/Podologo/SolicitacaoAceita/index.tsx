9706fc7c0057465e2fdb9cad16d290e29cef6e4d
Luiis96 <134606190+Luiis96@users.noreply.github.com>
2024-04-14 20:03:57 -0300

    Página InfoConsultaAceita concluída

--- podologos/src/screens/Private/Podologo/SolicitacaoAceita/index.tsx
+++ podologos/src/screens/Private/Podologo/SolicitacaoAceita/index.tsx
@@ -6,6 +6,7 @@ import ProfileInfo from "../../../../components/ProfileInfo";
 import { Button } from "../../../../components/Button";
 import FotoPe from "../../../../assets/FotoPe.png";
 import UserIcon from "../../../../assets/UserIcon.png";
+import InformacaoUsuario from "../../../../components/InformacaoUsuario";
 
 function SolicitacaoAceita() {
   return (
@@ -20,17 +21,11 @@ function SolicitacaoAceita() {
         <Text className="text-texto_cinza text-[18px] font-semibold">
           Informações do paciente
         </Text>
-        <View className="flex flex-row space-x-2">
-          <Image source={UserIcon}></Image>
-          <View className="flex justify-evenly">
-            <Text className="text-texto_cinza_claro">João de Oliveira</Text>
-            <Text className="text-texto_cinza_claro">(21) 12345-6789</Text>
-            <Text className="text-texto_cinza_claro">00000-000</Text>
-            <Text className="text-azul underline">Ver mais</Text>
-          </View>
-        </View>
+        <InformacaoUsuario></InformacaoUsuario>
         <View className="self-center w-[80%] border-b-[1px] opacity-10"></View>
-        <Text className="self-center text-azul">Aceita em 01/02/2024</Text>
+        <Text className="self-center text-azul text-[16px]">
+          Aceita em 01/02/2024
+        </Text>
         <View className="self-center w-[80%] border-b-[1px] opacity-10"></View>
         <Text className="text-texto_cinza text-[18px] font-semibold">
           Informações médicas
