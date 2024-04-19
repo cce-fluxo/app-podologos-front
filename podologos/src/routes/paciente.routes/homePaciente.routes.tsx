410eb03ccba816e70d216f93137265695a9d0b60
FelipeCassano <134609406+FelipeCassano@users.noreply.github.com>
2024-04-19 01:54:34 -0300

    .

---
+++ podologos/src/routes/paciente.routes/homePaciente.routes.tsx
@@ -0,0 +1,23 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import HomePodologo from "../../screens/Private/Podologo/HomePodologo";
+import InfoSolicitacaoConsulta from "../../screens/Private/Podologo/InfoSolicitacaoConsulta";
+
+const Stack = createStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="HomePodologo"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen
+        name="InfoSolicitacaoConsulta"
+        component={InfoSolicitacaoConsulta}
+      ></Stack.Screen>
+      <Stack.Screen name="HomePodologo" component={HomePodologo}></Stack.Screen>
+    </Stack.Navigator>
+  );
+}
+
+export default HomeStack;
