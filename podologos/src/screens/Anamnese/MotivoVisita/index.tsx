ee25e63d1ba4cf7dcfd3ef432d9e1fd250ec2bca
Luiis96 <134606190+Luiis96@users.noreply.github.com>
2024-05-27 10:33:56 -0300

    Página Anamnese Motivo da Visita criada

---
+++ podologos/src/screens/Anamnese/MotivoVisita/index.tsx
@@ -0,0 +1,91 @@
+import { SafeAreaView, ScrollView, Text } from "react-native";
+import Header from "../../../components/Header";
+import { FormData } from "../../../components/FormData/Index";
+import Input from "../../../components/Inputs";
+import { Button } from "../../../components/Button";
+
+export default function DadosPessoais() {
+  const onSubmit = (data: any) => {};
+
+  const column = [
+    {
+      name: "nome",
+      placeholder: "Nome",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "nascimetno",
+      placeholder: "Nascimento",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "cep",
+      placeholder: "CEP",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "endereco",
+      placeholder: "Endereço",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "bairro",
+      placeholder: "Bairro",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "cidade",
+      placeholder: "Cidade",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "uf",
+      placeholder: "UF",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "telefone",
+      placeholder: "Telefone",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "sexo",
+      placeholder: "Sexo",
+      className: "w-full",
+      component: Input,
+    },
+    {
+      name: "profissao",
+      placeholder: "Profissão",
+      className: "w-full",
+      component: Input,
+    },
+  ];
+
+  return (
+    <SafeAreaView className="h-full w-full flex flex-col items-center bg-branco">
+      <Header text="Ficha de anamnese"></Header>
+      <ScrollView className="w-full px-5">
+        <Text className="text-[20px] text-titulo_anamnese font-semibold mb-2">
+          Motivo da visita
+        </Text>
+        <FormData.Root onSubmit={onSubmit}>
+          <FormData.Form columns={column} id="formQuestion">
+            <Button
+              className="self-center mt-2 w-full"
+              placeholder="Continuar"
+            ></Button>
+          </FormData.Form>
+        </FormData.Root>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
