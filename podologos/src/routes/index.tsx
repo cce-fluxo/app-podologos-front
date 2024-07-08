cb8cae56d8eab4b2beea20075c4ae6c735ab71be
FelipeCassano <134609406+FelipeCassano@users.noreply.github.com>
2024-07-08 18:48:54 -0300

    Testando social routes

--- podologos/src/routes/index.tsx
+++ podologos/src/routes/index.tsx
@@ -2,11 +2,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import MainRoutes from "./main.routes";
 import PodologosRoutes from "./podologo.routes";
+import SocialRoutes from "./social.routes";
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <PodologosRoutes />
+      <SocialRoutes />
     </NavigationContainer>
   );
 }
--- podologos/src/routes/social.routes/index.tsx
+++ podologos/src/routes/social.routes/index.tsx
@@ -1,13 +1,13 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../../screens/SignedOut/Login";
 import PreLogin from "../../screens/SignedOut/PreLogin";
+import DenunciaPaciente from "../../screens/Private/Podologo/DenunciaPaciente";
+import CadastroPaciente from "../../screens/SignedOut/CadastroPaciente";
+import CadastroTeste from "../../screens/SignedOut/CadastroTeste";
+import CadastroPodologo from "../../screens/SignedOut/CadastroPodologo";
 
 const Stack = createStackNavigator();
-// onPress={() => {
-//   navigation.navigate("Perfil");
-// }}
-
-export default function StackRoutes() {
+export default function SocialRoutes() {
   return (
     <Stack.Navigator
       initialRouteName="PreLogin"
@@ -19,19 +19,55 @@ export default function StackRoutes() {
         options={{
           headerTitleAlign: "center",
           headerShown: false,
-          title: "Tela A",
         }}
       />
       <Stack.Screen
-        name="Login"
+        name="CadastroPaciente"
+        component={CadastroPaciente}
         options={{
-          title: "LoginPage",
+          headerBackTitleVisible: false, // Garante que a seta de volta seja visível
+          headerTintColor: "black", // Cor da seta de volta
+          title: "Nova Conta",
           headerTitleAlign: "center",
           headerStyle: {
-            backgroundColor: "white",
+            backgroundColor: "#FBFBFB",
+            height: 110,
+          },
+          headerTitleStyle: {
+            fontSize: 23,
+            color: "#0A284D",
+            fontWeight: "medium",
           },
         }}
+      />
+      <Stack.Screen
+        name="CadastroPodologo"
+        component={CadastroPodologo}
+        options={{
+          headerBackTitleVisible: false,
+          headerTintColor: "black",
+          title: "Nova Conta",
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#FBFBFB",
+            height: 110,
+          },
+          headerTitleStyle: {
+            fontSize: 23,
+            color: "#0A284D",
+            fontWeight: "medium",
+
+            // Ajuste o peso da fonte conforme necessário
+          },
+        }}
+      />
+
+      <Stack.Screen
+        name="Login"
         component={Login}
+        options={{
+          headerShown: false,
+        }}
       />
     </Stack.Navigator>
   );
