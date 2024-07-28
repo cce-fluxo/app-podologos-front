008f2ec8451bd83a79cbfe4384516ef775925ffb
FelipeCassano <134609406+FelipeCassano@users.noreply.github.com>
2024-07-28 20:34:52 -0300

    Aplicativo funcionando

--- podologos/src/components/ModalSimNao/index.tsx
+++ podologos/src/components/ModalSimNao/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Modal, Text, TouchableOpacity, View } from "react-native";
+import React from 'react';
+import { Modal, Text, TouchableOpacity, View } from 'react-native';
 
 interface PopupProps {
   modalVisible: boolean;
@@ -11,19 +11,19 @@ interface PopupProps {
 function ModalSimNao({ modalVisible, mensagem, onNoClick }: PopupProps) {
   return (
     <Modal transparent={true} visible={modalVisible}>
-      <View className="w-screen h-screen z-50 flex items-center justify-center fixed inset-0  bg-black-transparent">
-        <View className="bg-white w-[80vw] rounded-2xl shadow-sm shadow-black ">
-          <Text className="text-base text-center p-5 font-semibold">
+      <View className='fixed inset-0 z-50 flex h-screen w-screen items-center justify-center bg-black-transparent'>
+        <View className='w-[80vw] rounded-2xl bg-white shadow-sm shadow-black'>
+          <Text className='p-5 text-center text-base font-semibold'>
             {mensagem}
           </Text>
 
-          <View className="flex flex-row items-center justify-center border-t-[1px] border-zinc-300 ">
-            <TouchableOpacity onPress={onNoClick} className="mx-auto py-3">
-              <Text className="text-azul font-semibold text-lg">Sim</Text>
+          <View className='flex flex-row items-center justify-center border-t-[1px] border-zinc-300'>
+            <TouchableOpacity onPress={onNoClick} className='mx-auto py-3'>
+              <Text className='text-lg font-semibold text-azul'>Sim</Text>
             </TouchableOpacity>
-            <View className=" border-r-[1px] h-full border-zinc-300" />
-            <TouchableOpacity className="mx-auto py-3" onPress={onNoClick}>
-              <Text className="text-azul font-semibold text-lg">Não</Text>
+            <View className='h-full border-r-[1px] border-zinc-300' />
+            <TouchableOpacity className='mx-auto py-3' onPress={onNoClick}>
+              <Text className='text-lg font-semibold text-azul'>Não</Text>
             </TouchableOpacity>
           </View>
         </View>
