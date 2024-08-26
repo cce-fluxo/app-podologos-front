6918d9cd5592db0a892a34016b323b7a2d8d5c86
Luiis96 <134606190+Luiis96@users.noreply.github.com>
2024-08-26 01:28:59 -0300

    Rotas arrumadas

--- podologos/src/routes/social.routes/index.tsx
+++ podologos/src/routes/social.routes/index.tsx
@@ -7,7 +7,6 @@ import Email from '../../screens/SignedOut/ForgotPassword/Email';
 import Codigo from '../../screens/SignedOut/ForgotPassword/Codigo';
 import NovaSenha from '../../screens/SignedOut/ForgotPassword/NovaSenha';
 import FormacaoPodologo from '../../screens/SignedOut/FormacaoPodologo';
-import Avaliacao from '../../screens/Anamnese/Avaliacao';
 
 const Stack = createStackNavigator();
 
@@ -52,7 +51,7 @@ export default function SocialRoutes() {
       />
       <Stack.Screen
         name='CadastroPodologo'
-        component={Avaliacao}
+        component={CadastroPodologo}
         options={{
           headerBackTitleVisible: false,
           headerTintColor: '#0A284D',
