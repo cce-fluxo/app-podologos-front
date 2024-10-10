e8f779b3d326811bd2ae77984e024c64f2b7c34e
FelipeCassano <134609406+FelipeCassano@users.noreply.github.com>
2024-10-10 10:06:34 -0300

    Input

--- podologos/src/components/FormData/InputForm/index.tsx
+++ podologos/src/components/FormData/InputForm/index.tsx
@@ -22,6 +22,34 @@ function InputStyled({ field, placeholder, texto, form, ...rest }) {
   );
 }
 
+function InputPassword({
+  field,
+  placeholder,
+  texto,
+  form,
+  rightIcon,
+  ...rest
+}) {
+  return (
+    <View
+      {...rest}
+      className={`h-auto w-[87%] self-center ${texto === undefined ? 'gap-0' : ''}`}
+    >
+      <Text className={`${texto === undefined ? 'h-0' : 'h-auto'}`}>
+        {texto}
+      </Text>
+      <TextInput
+        onChangeText={form.handleChange(field.name)}
+        onBlur={form.handleBlur(field.name)}
+        value={form.values[field.name]}
+        placeholder={placeholder}
+        {...rest}
+        className='mt-2 h-14 rounded-[12px] bg-cinza/20 p-4 text-black'
+      ></TextInput>
+    </View>
+  );
+}
+
 const InputForm = styled(InputStyled);
 export default InputForm;
 
