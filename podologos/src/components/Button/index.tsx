83007f2648322005cafeacc49944ac8e8f1390a0
cqlucasufrj <lucascq.20222@poli.ufrj.br>
2025-02-20 15:19:47 -0300

    cadastro paciente - loading

--- podologos/src/components/Button/index.tsx
+++ podologos/src/components/Button/index.tsx
@@ -1,9 +1,10 @@
-import React, { Image, Text, TouchableOpacity } from 'react-native';
+import React, { ActivityIndicator, Image, Text, TouchableOpacity } from 'react-native';
 import { styled } from 'nativewind';
 
 function ButtonStyled({
   disabled,
   onPress,
+  loading,
   placeholder,
   text,
   imagem,
@@ -17,13 +18,17 @@ function ButtonStyled({
       className={`flex h-14 w-[90%] flex-row items-center justify-center ${disabled ? 'bg-botao_desabilitado' : 'bg-azul'} rounded-[12px]`}
       {...rest}
     >
-      {children}
-      <Image source={imagem} alt=''></Image>
-      <Text
-        className={`${text === undefined ? 'text-branco' : text} text-[18px]`}
-      >
-        {placeholder}
-      </Text>
+      {loading ? 
+      <ActivityIndicator size="large" color="#2087ED" /> 
+      :
+      <>{children}
+        <Image source={imagem} alt=''></Image>
+        <Text
+          className={`${text === undefined ? 'text-branco' : text} text-[18px]`}
+        >
+          {placeholder}
+        </Text>
+      </>}
     </TouchableOpacity>
   );
 }
