07b01399d266d1c7fdd013ab2042d836ceafc22d
cqlucasufrj <lucascq.20222@poli.ufrj.br>
2025-02-20 03:50:09 -0300

    componente - header bem vindo

--- podologos/src/screens/Private/Podologo/HomePodologo/index.tsx
+++ podologos/src/screens/Private/Podologo/HomePodologo/index.tsx
@@ -3,11 +3,13 @@ import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import Solicitacoes from '../../../../components/Solicitacoes';
 import { useNavigation } from '@react-navigation/native';
 import AuthContext from '../../../../context/AuthContext';
+import Header from '../../../../components/Header';
 
 function Home() {
   const { signed, user } = useContext(AuthContext);
   const navigation = useNavigation();
 
+  console.log(user);
   console.log('Estado signed mudou (HomeP):', signed);
 
   {
@@ -18,10 +20,11 @@ function Home() {
 
   return (
     <SafeAreaView className='flex w-screen flex-1 items-center space-y-10 bg-white'>
-      <View className='ml-10 mt-4 w-full justify-center'>
+      {/* <View className='ml-10 mt-8 w-full justify-center'>
         <Text className='text-[25px] text-azul_escuro'>Bem vindo,</Text>
-        <Text className='text-[25px] text-azul_escuro'>Luiz</Text>
-      </View>
+        <Text className='text-[25px] text-azul_escuro'>{user.first_name}</Text>
+      </View> */}
+      <Header bemVindo />
 
       <Text className='text-[25px] font-semibold text-[#46555A]'>
         Solicitações
